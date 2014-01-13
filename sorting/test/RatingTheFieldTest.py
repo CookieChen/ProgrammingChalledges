5d6d94a7e4dfe3393919c29e012954a3f1e164b3
CookieChen <orangedog227@gmail.com>
2014-01-13 12:00:29 +0800

    Isolate unit tests each other.

--- sorting/test/RatingTheFieldTest.py
+++ sorting/test/RatingTheFieldTest.py
@@ -8,40 +8,49 @@ import unittest
 from sorting.RatingTheField import *
 
 class Test(unittest.TestCase):
-    cookie = ""
-    eve = ""
 
     def setUp(self):
-        self.cookie = Suitor()
-        self.eve = Suitor()
+        pass
 
     def tearDown(self):
         pass
 
 
     def testHeightOfCompareSuitor(self):
-        self.cookie.height = 177
-        self.eve.height = 158
+        cookie = Suitor()
+        eve = Suitor()
+        
+        cookie.height = 177
+        eve.height = 158
 
-        self.assertTrue(compareSuitor(self.cookie, self.eve) == 1, "Height Of CompareSuitor()")
+        self.assertTrue(compareSuitor(cookie, eve) == 1, "Height Of CompareSuitor()")
         
     def testWeightOfCompareSuitor(self):
-        self.cookie.weight = 75
-        self.eve.weight = 51
+        cookie = Suitor()
+        eve = Suitor()
         
-        self.assertTrue(compareSuitor(self.eve, self.cookie) == -1, "Weight Of CompareSuitor()")
+        cookie.weight = 75
+        eve.weight = 51
+        
+        self.assertTrue(compareSuitor(eve, cookie) == -1, "Weight Of CompareSuitor()")
         
     def testLastNameOfCompareSuitor(self):
-        self.cookie.lastName = "Love"
-        self.eve.lastName = "Love"
+        cookie = Suitor()
+        eve = Suitor()
         
-        self.assertTrue(compareSuitor(self.cookie, self.eve) == 0, "LastName of CompareSuitor()")
+        cookie.lastName = "Love"
+        eve.lastName = "Love"
+        
+        self.assertTrue(compareSuitor(cookie, eve) == 0, "LastName of CompareSuitor()")
         
     def testFirstNameOfCompareSuitor(self):
-        self.cookie.firstName = "Cookie"
-        self.eve.firstName = "Cake"
+        cookie = Suitor()
+        eve = Suitor()
         
-        self.assertFalse(compareSuitor(self.cookie, self.eve) == 0, "LastName of CompareSuitor()")
+        cookie.firstName = "Cookie"
+        eve.firstName = "Cake"
+        
+        self.assertFalse(compareSuitor(cookie, eve) == 0, "LastName of CompareSuitor()")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
